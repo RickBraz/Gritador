f4fab7f8500302842f045508214217869da86b9a
= <=>
2020-05-15 00:53:09 -0300

    adicionei o programa

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola Usuario por favor digite uma frase!");
+ 
+            string frase = Console.ReadLine();
+            string mensagem = frase .ToUpper();
+
+            Console.WriteLine(mensagem);
+        }
+    }
+}
